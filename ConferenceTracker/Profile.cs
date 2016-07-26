9252b993c02a695990993de64a421e6c42221489
spencerharro <spencerharro@i2r.com>
2016-07-26 14:51:51 -0500

    New Profile and ProfileSetup classes

---
+++ ConferenceTracker/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConferenceTracker
+{
+    public class Profile
+    {
+        public string emailAddress { get; set; }
+        public string emailPassword { get; set; }
+        public string emailURL { get; set; }
+        public Room room { get; set; }
+    }
+}
---
+++ ConferenceTracker/ProfileSetup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConferenceTracker
+{
+    public class ProfileSetup
+    {
+
+        public Profile SetupRoomProfile(string roomName)
+        {
+            Profile profile = new Profile();
+
+            // Choose which profile based on room
+            switch (roomName)
+            {
+                case "North":
+
+                    //Setup room
+                    profile.room.RoomID = 1;
+                    profile.room.RoomName = "North Conference Room";
+
+                    //Exchange setup
+                    profile.emailAddress = "north@i2r.com";
+                    profile.emailPassword = "Catesuser1";
+                    profile.emailURL = "https://mex07a.emailsrvr.com/EWS/Exchange.asmx";
+
+                    break;
+                case "South":
+
+                    //Setup room
+                    profile.room.RoomID = 1; // 1 for North
+                    profile.room.RoomName = "South Conference Room";
+
+                    //Setup Exchange
+                    profile.emailAddress = "south@i2r.com";
+                    profile.emailPassword = "Catesuser1";
+                    profile.emailURL = "https://mex07a.emailsrvr.com/EWS/Exchange.asmx";
+
+                    break;
+                case "Training":
+
+                    //Setup room
+                    profile.room.RoomID = 1; // 1 for North
+                    profile.room.RoomName = "Training Room";
+
+                    //Setup Exchange
+                    profile.emailAddress = "training@i2r.com";
+                    profile.emailPassword = "Catesuser1";
+                    profile.emailURL = "https://mex07a.emailsrvr.com/EWS/Exchange.asmx";
+
+                    break;
+
+            };
+            return profile;
+        }
+    }
+}
